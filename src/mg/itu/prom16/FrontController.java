ec3065c6820dfd5127dccce5e35bbf053f81439a
MialyTianaRsm <mialy.rasamimanana@gmail.com>
2024-05-17 11:04:21 +0300

    features/sprint1

--- src/mg/itu/prom16/FrontController.java
+++ src/mg/itu/prom16/FrontController.java
@@ -1,32 +1,71 @@
 package mg.itu.prom16 ;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.util.Vector;
 
+
+import annotation.Controller;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import util.Utilitaire;
 
 public class FrontController extends HttpServlet {
-    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try (PrintWriter out = response.getWriter()) {
-          out.println("Tonga etooooo!");
-        } catch (Exception e) {
-            e.printStackTrace(response.getWriter());
+    String packagePath ; 
+    boolean isChecked ; 
+    Vector<String> controllers ; 
+
+    public void init(){
+        try{
+            controllers = new Utilitaire().getListController(this.getInitParameter("package"), Controller.class);
+            isChecked = true ; 
+        }catch(Exception e){
+            throw new RuntimeException();
         }
+    }
+ 
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException {
+        // try (PrintWriter out = response.getWriter()) {
+        //   out.println("Tonga etooooo!");
+        // } catch (Exception e) {
+        //     e.printStackTrace(response.getWriter());
+        // }
+        PrintWriter out = response.getWriter();
+        if(!isChecked){
+            init();
+        }else{
+            for(int i = 0 ; i < controllers.size() ; i++){
+                out.println(controllers.get(i));
+            }
+        }
+
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException | ServletException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException | ServletException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
 
