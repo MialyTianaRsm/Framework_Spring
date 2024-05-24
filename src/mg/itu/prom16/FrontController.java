fa13a160017c7e54dc1e378d8c887324ffe6add5
MialyTianaRsm <mialy.rasamimanana@gmail.com>
2024-05-24 09:32:09 +0300

    features/sprint2

--- src/mg/itu/prom16/FrontController.java
+++ src/mg/itu/prom16/FrontController.java
@@ -1,29 +1,43 @@
 package mg.itu.prom16 ;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.MalformedURLException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Vector;
 
-
 import annotation.Controller;
-import jakarta.servlet.ServletContext;
+import annotation.Get;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import util.Mapping;
 import util.Utilitaire;
 
 public class FrontController extends HttpServlet {
-    String packagePath ; 
-    boolean isChecked ; 
+    HashMap<String , Mapping> urlMapping ; 
     Vector<String> controllers ; 
 
     public void init(){
+        urlMapping = new HashMap<>();
         try{
-            controllers = new Utilitaire().getListController(this.getInitParameter("package"), Controller.class);
-            isChecked = true ; 
+            controllers = new Utilitaire().getList(this.getInitParameter("package"), Controller.class);
+        }catch(Exception e){
+            throw new RuntimeException();
+        }
+        try {
+            for(String controller : controllers){
+                Class<?> clazz = Class.forName(controller);
+                for (Method method : clazz.getDeclaredMethods()){
+                    if(method.isAnnotationPresent(Get.class)){
+                        Get getAnnotation = method.getAnnotation(Get.class);
+                        String url = getAnnotation.value(); 
+                        urlMapping.put(url , new Mapping(controller, method.getName()));
+                    }
+                }
+            }
         }catch(Exception e){
             throw new RuntimeException();
         }
@@ -36,12 +50,13 @@ public class FrontController extends HttpServlet {
         //     e.printStackTrace(response.getWriter());
         // }
         PrintWriter out = response.getWriter();
-        if(!isChecked){
-            init();
-        }else{
-            for(int i = 0 ; i < controllers.size() ; i++){
-                out.println(controllers.get(i));
-            }
+        String path = request.getRequestURI().substring(request.getContextPath().length());
+        Mapping mapping = urlMapping.get(path);
+        if(mapping != null){
+            out.println("url : "+path);
+            out.println("mapping : "+ mapping);
+        }else {
+            out.print("Pas de methode");
         }
 
     }
--- src/util/Utilitaire.java
+++ src/util/Utilitaire.java
@@ -2,6 +2,7 @@ package util ;
 
 import java.io.File;
 import java.lang.annotation.Annotation;
+import java.net.URL;
 import java.util.Vector;
 
 
@@ -12,30 +13,58 @@ public class Utilitaire {
         return path ; 
     }
 
-    public Vector<String> getListController(String packageName , Class<? extends Annotation> annotation) throws ClassNotFoundException {
-        Vector<String> controllers = new Vector<String>();
-        packageName.replace(".", "/");
-        try{
+    // public Vector<String> getList(String packageName , Class<? extends Annotation> annotation) throws ClassNotFoundException {
+    //     Vector<String> controllers = new Vector<String>();
+    //     packageName.replace(".", "/");
+    //     try{
 
-            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            java.net.URL source = classLoader.getResource(packageName);
+    //         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+    //         java.net.URL source = classLoader.getResource(packageName);
     
-            String realPath = this.modifyPath(source.getFile());
+    //         String realPath = this.modifyPath(source.getFile());
     
-            File classPathDirectory = new File(realPath);
-            if(classPathDirectory.isDirectory()){
-                packageName = packageName.replace("/", ".");
-                for(String fileName : classPathDirectory.list()){
-                    fileName = fileName.substring( 0,fileName.length()-6);
-                    String className = packageName +"."+ fileName ; 
-                    Class<?> clazz = Class.forName(className);
-                    if(clazz.isAnnotationPresent(annotation)){
-                        controllers.add(className);
-                    }
+    //         File classPathDirectory = new File(realPath);
+    //         if(classPathDirectory.isDirectory()){
+    //             packageName = packageName.replace("/", ".");
+    //             for(String fileName : classPathDirectory.list()){
+    //                 fileName = fileName.substring( 0,fileName.length()-6);
+    //                 String className = packageName +"."+ fileName ; 
+    //                 Class<?> clazz = Class.forName(className);
+    //                 if(clazz.isAnnotationPresent(annotation)){
+    //                     controllers.add(className);
+    //                 }
+    //             }
+    //         }
+    //     }catch(ClassNotFoundException e){
+    //         System.out.println(e.getMessage());
+    //     }
+    //     return controllers ; 
+        
+    // }
+    public Vector<String> getList(String packageName , Class<? extends Annotation> annotation) throws ClassNotFoundException {
+        Vector<String> controllers = new Vector<String>();
+        String path = packageName.replace(".", "/");
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        URL ressource = classLoader.getResource(path);
+        if( ressource == null ){
+            throw new ClassNotFoundException("Package = "+ packageName + "not found ");
+        }
+
+        String realPath = this.modifyPath(ressource.getFile());
+        File classPathDirectory = new File(realPath);
+        if(!classPathDirectory.exists() || !classPathDirectory.isDirectory()){
+            throw new ClassNotFoundException("Package = "+ packageName + "not found ");
+        }
+
+        for(String fileName : classPathDirectory.list()){
+            if(fileName.endsWith(".class")){
+                String className = packageName + "." +fileName.substring(0 , fileName.length()-6);
+                Class<?> clazz = Class.forName(className);
+                if(clazz.isAnnotationPresent(annotation)){
+                    controllers.add(className);
                 }
+
             }
-        }catch(ClassNotFoundException e){
-            System.out.println(e.getMessage());
         }
         return controllers ; 
         
