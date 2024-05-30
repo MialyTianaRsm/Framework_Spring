bec530aa2f10f4495e50fcad6833fcd14f85d13a
MialyRsm <mialy.rasamimanana@gmail.com>
2024-05-30 17:05:59 +0300

    wip: Working on sprint-2

--- src/mg/itu/prom16/FrontController.java
+++ src/mg/itu/prom16/FrontController.java
@@ -1,11 +1,10 @@
-package mg.itu.prom16 ;
+package mg.itu.prom16;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.util.Vector;
-
 
 import annotation.Controller;
 import jakarta.servlet.ServletContext;
@@ -16,30 +15,26 @@ import jakarta.servlet.http.HttpServletResponse;
 import util.Utilitaire;
 
 public class FrontController extends HttpServlet {
-    String packagePath ; 
-    boolean isChecked ; 
-    Vector<String> controllers ; 
+    String packagePath;
+    boolean isChecked;
+    Vector<String> controllers;
 
-    public void init(){
-        try{
+    public void init() {
+        try {
             controllers = new Utilitaire().getListController(this.getInitParameter("package"), Controller.class);
-            isChecked = true ; 
-        }catch(Exception e){
+            isChecked = true;
+        } catch (Exception e) {
             throw new RuntimeException();
         }
     }
- 
-    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException {
-        // try (PrintWriter out = response.getWriter()) {
-        //   out.println("Tonga etooooo!");
-        // } catch (Exception e) {
-        //     e.printStackTrace(response.getWriter());
-        // // }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException {
         PrintWriter out = response.getWriter();
-        if(!isChecked){
+        if (!isChecked) {
             init();
-        }else{
-            for(int i = 0 ; i < controllers.size() ; i++){
+        } else {
+            for (int i = 0; i < controllers.size(); i++) {
                 out.println(controllers.get(i));
             }
         }
@@ -67,6 +62,5 @@ public class FrontController extends HttpServlet {
             e.printStackTrace();
         }
     }
-
 
 }
